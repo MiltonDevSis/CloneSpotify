b58623514883ce389316816d36cdc837a43c607d
MiltonDevSis <milton.dev.sis@gmail.com>
2021-05-19 19:55:39 -0300

    SmartTabLayout

--- app/src/main/java/com/example/myspotify/fragments/Biblioteca.kt
+++ app/src/main/java/com/example/myspotify/fragments/Biblioteca.kt
@@ -1,11 +1,17 @@
 package com.example.myspotify.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.myspotify.R
+import com.example.myspotify.fragmentsTab.Albuns
+import com.example.myspotify.fragmentsTab.Artistas
+import com.example.myspotify.fragmentsTab.Playlists
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+import kotlinx.android.synthetic.main.fragment_biblioteca.*
 
 class Biblioteca : Fragment() {
 
@@ -23,5 +29,17 @@ class Biblioteca : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_biblioteca, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val adapter = FragmentPagerItemAdapter(fragmentManager, FragmentPagerItems.with(context)
+            .add("Playlists", Playlists::class.java)
+            .add("Artistas", Artistas::class.java)
+            .add("Álbuns", Albuns::class.java)
+            .create())
+
+        viewpager.adapter = adapter
+        viewpagertab.setViewPager(viewpager)
     }
 }
